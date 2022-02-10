2e13729a31185d9321e525892f2d23a4ab7578bc
SP1N4X <82057630+SP1N4X@users.noreply.github.com>
2022-02-10 16:37:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import requests
 
-response = requests.get('http://cal-prob-spina.herokuapp.com')
-print(response.text)
+headers = {"Referer": "https://www.betexplorer.com",
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'}
+s = requests.Session()
+session = s.headers.update(headers)
+
+request = s.get('https://www.sisal.it/api-betting/vrol-api/vrol/palinsesto/getAlberaturaEventiSingoli/1')
+response = request.json()
+
+print(response)
 
 print('ale')
