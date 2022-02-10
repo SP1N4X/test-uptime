726e6c18874439bc427148f818d8c59cc2c32f04
SP1N4X <82057630+SP1N4X@users.noreply.github.com>
2022-02-10 15:21:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-from flask import Flask, request
-import cal_result
+import requests
 
-app = Flask('')
-
-
-@app.route('/')
-
-def index():
-    return "<h1>Welcome to our server !!</h1>"
-
-
-@app.route('/post', methods=['POST'])
-
-def result():
-    ale = request.get_json()
-    risultato = cal_result.cal(ale)
-    return risultato
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36',
+    "Accept-Encoding": "gzip, deflate, br", 
+    "Accept": "application/json, text/plain, */*",
+    "DNT": "1", 
+    "Connection": "keep-alive", 
+    "Upgrade-Insecure-Requests": "1"
+    }
+response = requests.get('https://www.sisal.it/api-betting/vrol-api/vrol/palinsesto/getAlberaturaEventiSingoli/1', headers=headers)
+print(response.json())
