48f8b30a050944ddfae3d197c1306df893a997ff
SP1N4X <82057630+SP1N4X@users.noreply.github.com>
2022-02-11 16:05:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const axios = require('axios')
 
+let config = {
+  headers: {
+    'Accept': 'application/json, text/plain, */*',
+    'User-Agent': 'axios/0.25.0',
+    'Host': 'www.sisal.it',
+    'Connection': 'close'
+  }
+}
+
 axios
-  .get('https://www.sisal.it/api-betting/vrol-api/vrol/palinsesto/getAlberaturaEventiSingoli/1')
+  .get('https://www.sisal.it/api-betting/vrol-api/vrol/palinsesto/getAlberaturaEventiSingoli/1', config)
   .then(res => {
     console.log(`statusCode: ${res.status}`)
     console.log(res)
